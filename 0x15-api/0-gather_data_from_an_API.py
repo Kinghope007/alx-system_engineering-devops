1f3b66c55eda53d3b680da6ce2e0e778adf45870
Kinghope007 <emmanuelhope39@gmail.com>
2024-02-19 20:21:44 -0800

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -42,6 +42,8 @@ if __name__ == "__main__":
     completed_tasks = [task for task in todos_data if task.get('completed')]
     num_completed_tasks = len(completed_tasks)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, total_tasks))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_completed_tasks, total_tasks))
+
     for task in completed_tasks:
         print("\t {}".format(task.get('title')))
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
-"""
-Script to gather data from an API and export to JSON
-"""
-
-import requests
+""" Python to get data from an API and convert to Json"""
+import csv
 import json
+import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
 
-    employee_id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = '{}/users/{}'.format(base_url, employee_id)
-    todos_url = '{}/todos?userId={}'.format(base_url, employee_id)
+if __name__ == '__main__':
+    USER_ID = sys.argv[1]
+    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+    res = requests.get(url_to_user)
+    """Documentation"""
+    USERNAME = res.json().get('username')
+    """Documentation"""
+    url_to_task = url_to_user + '/todos'
+    res = requests.get(url_to_task)
+    tasks = res.json()
 
-    try:
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
-    except Exception as e:
-        print("Error: {}".format(e))
-        sys.exit(1)
-
-    if user_response.status_code != 200:
-        print("Error: User data not found")
-        sys.exit(1)
-
-    if todos_response.status_code != 200:
-        print("Error: TODO list not found")
-        sys.exit(1)
-
-    employee_name = user_data.get('username')
-    if not employee_name:
-        print("Error: Employee name not found")
-        sys.exit(1)
-
-    filename = '{}.json'.format(employee_id)
-    data = {employee_id: [{"task": task.get('title'), "completed": task.get('completed'), "username": employee_name} for task in todos_data]}
-    with open(filename, 'w') as file:
-        json.dump(data, file)
+    dict_data = {USER_ID: []}
+    for task in tasks:
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+        dict_data[USER_ID].append({
+                                  "task": TASK_TITLE,
+                                  "completed": TASK_COMPLETED_STATUS,
+                                  "username": USERNAME})
+    """print(dict_data)"""
+    with open('{}.json'.format(USER_ID), 'w') as f:
+        json.dump(dict_data, f)
